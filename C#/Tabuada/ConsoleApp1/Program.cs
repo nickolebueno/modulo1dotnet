2302c79d55db76f51516cf746983074413a4f957
Nickole Bueno <102301475+nickolebueno@users.noreply.github.com>
2022-04-06 17:45:24 -0300

    atividade 6/04

---
+++ C#/Tabuada/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= 10; i++)
+            {
+            Console.WriteLine($"{i} x {input} = {i * input}");
+            }
+
+        }
+    }
+}
