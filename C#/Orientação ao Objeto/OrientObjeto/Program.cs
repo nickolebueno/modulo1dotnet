a87f6c814890f66e88b698a859203f258c1e0444
Nickole Bueno <102301475+nickolebueno@users.noreply.github.com>
2022-04-08 19:22:19 -0300

    atividade 08.04

---
+++ C#/Orientação ao Objeto/OrientObjeto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OrientObjeto.src;
+
+namespace OrientObjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Amor harry = new Amor("comfortable", "husband", "in love");
+            Amor songs = new Amor("as it was", "watermelon sugar", "falling");
+            Amor gossip = new Amor("one direction", "louis", "never go out of style");
+            Amor about = new Amor("bi", "handsome", "mine");
+
+            Console.WriteLine("leitura de objetos");
+            harry.Exibir();
+            songs.Exibir();
+            gossip.Exibir();
+            about.Exibir();
+
+   
+
+        }
+    }
+}
---
+++ C#/Orientação ao Objeto/OrientObjeto/src/Amor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientObjeto.src
+{
+    public class Amor
+    {
+        public string Sentimento { get; set; }
+        public string Familia { get; set; }
+        public string Sensação  { get; set; }
+
+        public Amor() { }
+        public Amor(string sensação, string familia, string sentimento) 
+        
+        {
+            Sensação = sensação;
+            Familia = familia;
+            Sentimento = sentimento;
+        }
+
+        public void Sentir (string Emoção)
+        {
+
+            Console.WriteLine($"Você está sentindo:{Emoção} ");
+        }
+
+        public void Exibir () 
+        {
+            Console.WriteLine($"sentimento: {Sentimento}");
+            Console.WriteLine($"familia: {Familia}");
+            Console.WriteLine($"sensação: {Sensação}");
+
+        }
+    }
+}
